25d1a9607044d6ec47f5e47b770349d0a565e7fd
moby989 <46134028+moby989@users.noreply.github.com>
2019-04-17 23:32:30 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 23:20:06 2019
+
+@author: moby
+"""
+
+from Airbnb_Spyder import Airbnb_spyder
+from Cookies import url_other_properties
+
+spyder = Airbnb_spyder(url_other_properties)
+
+payload = {'price_min':10,'price_max':11}
+
+r = spyder.get_r(spyder.url,payload)          
+data = r.json()
+
+
+print (r.url)
+#print (dict(r.cookies))
+print (r.headers)
+property = data['explore_tabs'][0]['sections'][0]['listings'][0]['pricing_quote']
+print (property)
+
+        
