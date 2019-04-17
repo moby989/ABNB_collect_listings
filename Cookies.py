60a3b8cf63bff3ac3e29acd037d2553a9114b4f8
moby989 <46134028+moby989@users.noreply.github.com>
2019-04-17 19:03:13 +0800

    Update Cookies.py

--- Cookies.py
+++ Cookies.py
@@ -25,9 +25,9 @@ https://www.airbnb.ru/api/v2/homes_pdp_reviews?currency=USD&key=d306zoyjsyarp7if
 
 
 
-headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0)\
-          AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 \
-          Safari/537.36'}        
+headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)\
+           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 \
+           Mobile Safari/537.36'}     
 
 #update cookies to use in cloud version
 
