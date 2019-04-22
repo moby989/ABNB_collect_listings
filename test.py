e6794e771c98c1886717c515ba477c9009db99de
moby989 <46134028+moby989@users.noreply.github.com>
2019-04-22 23:57:16 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,20 +9,35 @@ Created on Wed Apr 17 23:20:06 2019
 from Airbnb_Spyder import Airbnb_spyder
 from Cookies import url_other_properties
 
-spyder = Airbnb_spyder(url_other_properties)
 
-payload = {'price_min':10,'price_max':11,'adults':2}
+url2 = 'https://www.whatismybrowser.com'
+
+
+spyder = Airbnb_spyder(url_other_properties)
+spyder2 = Airbnb_spyder(url2)
+
+
+#payload = {'price_min':10,'price_max':11}
 
 r = spyder.get_r(spyder.url)          
 data = r.json()
 
 
+
+r2 = spyder.get_r(spyder2.url)
+
+
+
 print (r.url)
 #print (dict(r.cookies))
 print (r.headers)
-property = data['explore_tabs'][0]['sections'][1]['listings'][0]['pricing_quote']
+property = data['explore_tabs'][0]['sections'][0]['listings'][0]['pricing_quote']
 print (property)
-# print (data)
 
+k = r.cookies
+
+ 
+print (dict(k))
+print (r2.content)
 
         
