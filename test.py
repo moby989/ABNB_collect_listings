6eb9168698df3deb9bea6422c5a8bb810ad92581
moby989 <46134028+moby989@users.noreply.github.com>
2019-04-24 16:57:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ data = r.json()
 #print (r.url)
 #print (dict(r.cookies))
 print (r.headers)
-property = data['explore_tabs'][0]['sections'][0]['listings'][0]['pricing_quote']
+property = data['explore_tabs'][0]['sections'][0]
 property2 = data['explore_tabs'][0]['sections'][1]['listings'][0]['pricing_quote']
 property3 = data['explore_tabs'][0]['sections']
 print (property)
