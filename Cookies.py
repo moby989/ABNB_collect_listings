8213dde9c75a8b16093a265b060b0f97269e66b3
moby989 <46134028+moby989@users.noreply.github.com>
2019-04-24 23:29:40 +0800

    Add files via upload

--- Cookies.py
+++ Cookies.py
@@ -22,13 +22,10 @@ https://www.airbnb.ru/api/v2/homes_pdp_reviews?currency=USD&key=d306zoyjsyarp7if
 
 """
 
-headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) \
-           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
-'accept': 'application/json, text/javascript, */*; q=0.01',
-'accept-encoding': 'gzip, deflate, br',
-'save-data': 'on',
-'x-csrf-token': 'V4$.airbnb.com$V5KUmGhJ0AQ$yjbviXB2lQjGp5qffckUt_0dU4TTOiQrQ-ZL9OsFBCM=',
-'x-requested-with': 'XMLHttpRequest'}
+#headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) \
+#          AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0'}
 
 cookies = [
 
