497c72a18c770006905789902329172ab37b8d4e
moby989 <46134028+moby989@users.noreply.github.com>
2019-08-05 20:06:50 +0300

    Add files via upload

--- run_Airbnb_Spyder_ver1.0.py
+++ run_Airbnb_Spyder_ver1.0.py
@@ -37,47 +37,61 @@ def collectDb(scrap_date,db = db):
     collects and uploads to the server "listings" data
     requests to Airbnb made for each price range (~300 properties per one request)
     
+    property_types (collected separately):
+    1) ENTIREHomeVILLAS - Villas only
+    2) EntireHomeNONVILLAS - all private properties except Villas
+    3) OTHER - other non-private properties        
+    
     """   
-    #load variables
+    #load variables (URLs)
     collection = db.VARIABLES            
-    ptypes = collection.find_one()['url_ptype']
+    ptypes = collection.find_one()['url_ptype2']
 
-
+    #collect listings data for each property type
     for ptype in ptypes:
         ms = AS(ptypes[ptype].strip('﻿')) 
              
         #query for histogram
-        collection = db.histogram
-        histogram_cursor  = collection.find({'scrap_date':scrap_date,
-                                             'parsed':False})
-        histogram = [h for h in histogram_cursor]   
-#        print(histogram)                  
+        collection_hist = db.histogram
+        histogram_cursor  = collection_hist.find({'scrap_date':scrap_date,
+                                                  'parsed':False,
+                                                  'ptype':ptype})
 
-        #collect property db
+        histogram = [h for h in histogram_cursor]
+
+        #collect property db and real histogram
         scraping_results = ms.collect_db(ptype,histogram)
-        listings = scraping_results[0]
-        
-        hist_actual = scraping_results[1]
+        listings = scraping_results[0]        
+        hist_actual = scraping_results[1]        
+
         for h in hist_actual:
             h['ptype'] = ptype
-        print(hist_actual)
-        
-#        return (hist_actual)
-    
+            
         #upload to the server
-        collection = db.listings
-        collection.insert_many(listings)
+        collection_listings = db.listings
+        collection_listings.insert_many(listings)
 
-        #updating histogram on the server
-        collection = db.histogram
+        #updating histogram collection on the server
         for raw in hist_actual:                    
-            collection.find_one_and_update(
-                    {'minimum_price':raw['minimum_price'],'ptype':ptype},
+            collection_hist.find_one_and_update(
+                    {'minimum_price':raw['minimum_price'],
+                     'ptype':ptype},
                     {'$set': 
                             {'n_actual':raw['n_properties'],
-                             'parsed':True                          }
-                                                                     }
+                             'parsed':True}}
                                             )
+    #clean histogram                                                                
+    collection_hist.update_many(
+                    {'scrap_date':scrap_date},
+                    {'$set': 
+                            {'parsed':False}})
+        
+        
+    #update listings
+    collection_listings.update_many(
+                    {'scrap_date':scrap_date},
+                    {'$set': 
+                            {'scrap_date':scrap_date}}) 
         
     return print('Job done')
 
@@ -107,6 +121,7 @@ def collectHistogram():
                          
     return None    
 
-hist = collectHistogram()
 df = collectDb('2019-08-05')
+
+hist = collectHistogram()
 
